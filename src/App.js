5c272f8c13afb558d5cba3eda187c5f032c59ef5
Charly <alctagne@gmail.com>
2021-08-15 15:33:40 -0400

    setup src folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.CSS';
+import Header from './components/Header';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Index from './components/pages/Index';
+import Work from './components/pages/Work';
+import Contact from './components/pages/Contact';
+import About from './components/pages/About';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router basename='/reactportfolio'>
+      <Header />
+      <Switch>
+        <Route path='/' exact component={Index}/>
+        <Route path='/Work' exact component={Work}/>
+        <Route path='/Contact' exact component={Contact}/>
+        <Route path='/About' exact component={About}/>
+      </Switch>
+    </Router>
+    
+  </>
   );
 }
+
+export default App;
 
 export default App;
