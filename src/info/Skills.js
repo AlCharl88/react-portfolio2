c10e59eb20ec0415954aeba8dffed3fd39636fcf
Charly <alctagne@gmail.com>
2021-08-16 20:47:22 -0400

    setup skills

---
+++ src/info/Skills.js
@@ -0,0 +1,59 @@
+import react from '../pictures/react.png';
+import mongodb from '../pictures/mongodb.png';
+import mysql from '../pictures/mysql.png';
+import nodejs from '../pictures/nodejs.png';
+import express from '../pictures/express.png';
+import javascript from '../pictures/javascript.png';
+import html from '../pictures/html.png';
+import css from '../pictures/css.png';
+import bootstrap from '../pictures/bootstrap.png';
+
+const skills = [
+    {
+        "id": 1,
+        "skill": "React.js",
+        "src": react
+    },
+    {
+        "id": 2,
+        "skill": "MongoDB",
+        "src": mongodb
+    },
+    {
+        "id": 3,
+        "skill": "MySQL",
+        "src": mysql
+    },
+    {
+        "id": 4,
+        "skill": "Node.js",
+        "src": nodejs
+    },
+    {
+        "id": 5,
+        "skill": "Express",
+        "src": express
+    },
+    {
+        "id": 6,
+        "skill": "JavaScript",
+        "src": javascript
+    },
+    {
+        "id": 7,
+        "skill": "HTML",
+        "src": html
+    },
+    {
+        "id": 8,
+        "skill": "CSS",
+        "src": css
+    },
+    {
+        "id": 9,
+        "skill": "Bootstrap",
+        "src": bootstrap
+    }
+];
+
+export default skills;
