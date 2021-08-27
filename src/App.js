c024e290f6670e3e2a5c220751115827862d8bdc
Charly <alctagne@gmail.com>
2021-08-26 23:20:12 -0400

    code update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+import {Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "./Components/Components/Navbar";
-import { Container } from 'reactstrap';
+// import { Container } from 'reactstrap';
 import About from './Components/Pages/AboutMe';
 import Projects from './Components/Pages/Projects';
 import Contact from './Components/Pages/Contact';
@@ -15,12 +15,14 @@ library.add(faEnvelope, faCodeBranch);
 function App() {
   return (
       <div className="App">
+        <Router basename='/react-portfolio2/' >
         <Navbar />
-          <Container>
-            <Route exact path="/" component={About} />
-            <Route path="/projects" component={Projects} />
-            <Route path="/contact" component={Contact} />
-          </Container>
+          <Switch>
+            <Route path="/" exact component={About}/>
+            <Route path="/projects" exact component={Projects}/>
+            <Route path="/contact" exact component={Contact}/>
+            </Switch>
+          </Router>
       </div>
   );
 }
